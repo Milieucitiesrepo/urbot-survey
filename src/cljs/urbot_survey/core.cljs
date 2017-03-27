1d2bb2a58ab3893f055f6ea03351a48b74bc2fe9
Tom Goldsmith <goldsmith.tee@gmail.com>
2017-03-26 21:27:09 -0400

    Got mobile working.

--- src/cljs/urbot_survey/core.cljs
+++ src/cljs/urbot_survey/core.cljs
@@ -12,42 +12,37 @@
 
 (def debug? config/debug?)
 
-(defn- mobile?
-  []
-  (<= (.-innerWidth js/window) 800))
-
 (defn mount-roots []
-  (when-not (mobile?)
-    (re-frame/clear-subscription-cache!)
+  (re-frame/clear-subscription-cache!)
 
-    (flow/init!
-     {:on-window-did-resize (fn [size] (re-frame/dispatch [:window-did-resize size]))
-      :on-window-did-scroll (fn [] (re-frame/dispatch [:window-did-scroll (flow/window-offset)]))})
+  (flow/init!
+   {:on-window-did-resize (fn [size] (re-frame/dispatch [:window-did-resize size]))
+    :on-window-did-scroll (fn [] (re-frame/dispatch [:window-did-scroll (flow/window-offset)]))})
 
-    (let [completed-surveys-set @(re-frame/subscribe [:completed-surveys])]
-      (doseq [element (array-seq (.getElementsByClassName js/document "urbot-survey"))]
-        (let [attrs (->> (array-seq (.-attributes element))
-                         (map (fn [attr]
-                                [(.-name attr)
-                                 (.-value attr)]))
-                         (into {})
-                         (map-keys ->keyword))
-              allow-redo? (-> attrs :data-allow-redo (= "true"))
-              attrs (update-in attrs
-                               [:data-survey-urls]
-                               (fn [survey-urls]
-                                 (->> survey-urls
-                                      (.parse js/JSON)
-                                      js->clj
-                                      (remove
-                                       (if (or debug? allow-redo?)
-                                         #{}
-                                         completed-surveys-set)))))]
-          (if (not-empty (:data-survey-urls attrs))
-            (reagent/render
-             [views/main-panel
-              {:data attrs}] element)
-            (.warn js/console "urbot.survey: No incomplete surveys available.")))))))
+  (let [completed-surveys-set @(re-frame/subscribe [:completed-surveys])]
+    (doseq [element (array-seq (.getElementsByClassName js/document "urbot-survey"))]
+      (let [attrs (->> (array-seq (.-attributes element))
+                       (map (fn [attr]
+                              [(.-name attr)
+                               (.-value attr)]))
+                       (into {})
+                       (map-keys ->keyword))
+            allow-redo? (-> attrs :data-allow-redo (= "true"))
+            attrs (update-in attrs
+                             [:data-survey-urls]
+                             (fn [survey-urls]
+                               (->> survey-urls
+                                    (.parse js/JSON)
+                                    js->clj
+                                    (remove
+                                     (if (or debug? allow-redo?)
+                                       #{}
+                                       completed-surveys-set)))))]
+        (if (not-empty (:data-survey-urls attrs))
+          (reagent/render
+           [views/main-panel
+            {:data attrs}] element)
+          (.warn js/console "urbot.survey: No incomplete surveys available."))))))
 
 (defn ^:export init []
   (re-frame/dispatch-sync [:initialize-db])
